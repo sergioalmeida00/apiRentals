5a5ff9aa1aed86d80de3a5b9039dd79a1a094baf
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2022-06-20 21:52:33 -0300

    delete consoleLog

--- src/modules/cars/useCases/ListCategories/ListCategoriesController.spec.ts
+++ src/modules/cars/useCases/ListCategories/ListCategoriesController.spec.ts
@@ -45,7 +45,7 @@ describe("List Categories",()=>{
             });
 
         const response = await request(app).get("/categories");
-            console.log(response.body)
+        
         expect(response.status).toBe(201);
         expect(response.body.length).toBe(1);
         expect(response.body[0]).toHaveProperty("id");
