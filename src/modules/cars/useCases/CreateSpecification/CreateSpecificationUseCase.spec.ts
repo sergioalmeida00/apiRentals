b71b0b0d56dec86318a990a38cb42b13df19e50f
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2022-09-28 05:46:28 -0300

    Feature: Create Test CreateSpecificationUseCase

---
+++ src/modules/cars/useCases/CreateSpecification/CreateSpecificationUseCase.spec.ts
@@ -0,0 +1,38 @@
+import { SpecificationsRepositoryInMemory } from "@modules/cars/repositories/in-memory/SpecificationsRepositoryInMemory";
+import { AppError } from "@shared/erros/AppError";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+
+
+let specificationsRepositoryInMemory: SpecificationsRepositoryInMemory;
+let createSpecificationUseCase:CreateSpecificationUseCase;
+describe("Create Specification", () => {
+    beforeAll(() => {
+        specificationsRepositoryInMemory = new SpecificationsRepositoryInMemory();
+        createSpecificationUseCase = new CreateSpecificationUseCase(specificationsRepositoryInMemory);
+    });
+
+    it("should be able create a specification" , async () => {
+        const specification = await createSpecificationUseCase.execute({
+            description_specification:"George Hamilton",
+            name_specification:"Brett Woods"
+        });
+
+        expect(specification).toHaveProperty('id_specification');
+        expect(specification.name_specification).toEqual("Brett Woods");
+    });
+
+    it("should not be able  create specification name existing", async () => {
+
+        await expect(async ()=>{
+            await createSpecificationUseCase.execute({
+                description_specification:"George Hamilton",
+                name_specification:"Brett Woods"
+            });
+            await createSpecificationUseCase.execute({
+                description_specification:"George Hamilton",
+                name_specification:"Brett Woods"
+            });
+        }).rejects.toBeInstanceOf(AppError)
+    })
+})
--- src/modules/cars/useCases/ListCategories/index.ts
+++
@@ -1,9 +0,0 @@
-// import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
-// import { ListCategoriesController } from "./ListCategoriesController";
-// import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
-
-// const categoriesRepositories = null;
-// const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepositories);
-// const listCategoriesController = new ListCategoriesController(listCategoriesUseCase);
-
-// export {listCategoriesController}
