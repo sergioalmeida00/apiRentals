2b8c11a196441bd5acc0e2955cb66fc7ad9ece8c
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2022-10-11 07:12:06 -0300

    Feature: Database

--- src/shared/infra/typeorm/index.ts
+++ src/shared/infra/typeorm/index.ts
@@ -4,12 +4,11 @@ interface IOptions {
   host: string;
 }
 
-export default async (host = "database_ignite"):Promise<Connection> =>{
+export default async ():Promise<Connection> =>{
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions,{
-      host: process.env.NODE_ENV === "test" ? "localhost" : host,
       database: process.env.NODE_ENV === "test" ? "rentx_test" : defaultOptions.database,
     })
   );
