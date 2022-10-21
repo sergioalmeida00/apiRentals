a44b446efe904aa11adec801a63f4dbd9d5ea280
sergioalmeida00 <sergioalmeidaa00@gmail.com>
2022-10-21 16:10:57 -0300

    Feature: app adjust

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -8,16 +8,14 @@ import * as Tracing from "@sentry/tracing";
 import "@shared/container";
 import upload from "@config/upload";
 import { AppError } from "@shared/erros/AppError";
-import { rateLimiter } from "@shared/infra/http/middlewares/rateLimiter"
+import { router } from '@shared/infra/http/routes/index';
 import createConnection from '@shared/infra/typeorm';
 import swaggerFile from '../../../swagger.json';
-import { router } from '@shared/infra/http/routes/index';
+import { rateLimiter } from "@shared/infra/http/middlewares/rateLimiter"
 
 
-createConnection();
 const app = express();
-
-app.use(rateLimiter);
+createConnection();
 
 Sentry.init({
     dsn: process.env.SENTRY_DSN ,
@@ -32,12 +30,10 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 
 app.use(express.json());
-
 app.use("/api-docs",swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
 app.use("/avatar",express.static(`${upload.tmpFolder}/avatar`));
 app.use('/cars',express.static(`${upload.tmpFolder}/cars`));
-
+app.use(rateLimiter);
 app.use(router);
 
 app.use(Sentry.Handlers.errorHandler());
